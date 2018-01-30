a9e2d8c7653e65e1f2ee2718482bc888a90c2ad5
ykiu <32252655+ykiu@users.noreply.github.com>
2018-01-30 20:04:10 +0900

    Bump up version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-nested-forms',
-    version='0.1.0',
+    version='0.2.0',
     description='A util to make Django formsets easy to use.',
     packages=find_packages(include=['nested_forms']),
     install_requires=['django >= 1.5, <2'],
